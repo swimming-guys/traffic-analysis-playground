b88881b4901c41a5f488a76e9833855c47f90df4
asingley <singley.a@husky.neu.edu>
2020-03-09 23:50:00 -0400

    Drawing at arbitary thetas is possible now

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ class Road extends React.Component {
 class Car extends React.Component {
 
     state = {
-        image: null
+        image: null,
     };
 
     componentDidMount() {
@@ -50,17 +50,53 @@ class Car extends React.Component {
                 image: image
             });
         };
+
+        /* var parent = this._reactInternalInstance._currentElement._owner._instance;
+
+         * var anim = new Konva.Animation(function(frame) {
+         *     var dist = 50 * (frame.timeDiff / 1000);
+         *     this.image.move({x: dist, y: 0});
+         * }, parent);
+
+         * anim.start(); */
+    }
+
+    centerY = window.innerHeight / 2
+    centerX = window.innerWidth / 2
+    carSize = this.centerY * .15
+    lane1Radius = this.centerX * .487
+    carCenter = this.carSize / 2
+    lanes = [this.lane1Radius]
+
+
+
+    getX(locationRadian) {
+        return this.centerX + (this.lanes[this.props.lane - 1] * Math.cos(locationRadian));
+    }
+
+    getY(locationRadian) {
+        return this.centerY + (this.lanes[this.props.lane - 1] * Math.sin(locationRadian));
+    }
+
+
+    getRotation(theta) {
+        return theta * (180/Math.PI);
     }
 
     render() {
-        const wih = window.innerHeight
+        this.locationRadian = this.props.radian * Math.PI
+        this.rotationRadian = this.locationRadian + 1.5 * Math.PI
+
         return (
             <Image
                 image={this.state.image}
-                x={(window.innerWidth / 2) - (wih * .05)}
-                y={( wih / 2) + (wih * .265)}
-                height={wih * .1}
-                width={wih * .1}
+                x={this.getX(this.locationRadian)}
+                y={this.getY(this.locationRadian)}
+                height={this.carSize}
+                width={this.carSize}
+                rotation={this.getRotation(this.rotationRadian)}
+                offsetX={this.carCenter}
+                offsetY={this.carCenter}
             />
         );
     }
@@ -76,14 +112,20 @@ class App extends Component {
     // And then we have canvas shapes inside the Layer
     return (
       <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer >
+        <Layer id="klayer" >
           <Text text="Try click on rect" />
           <Road radius=".8" color="#AAAAAA" />
           <Road radius=".78" color="#444444" />
           <Road radius=".68" color="#AAAAAA" lane="true" />
           <Road radius=".58" color="#AAAAAA" />
           <Road radius=".56" color="green" />
-          <Car />
+          <Car radian="1.5" lane="1" />
+          <Car radian="1" lane="1" />
+          <Car radian=".75" lane="1" />
+          <Car radian=".25" lane="1" />
+          <Car radian="2" lane="1" />
+          <Car radian="1.25" lane="1" />
+          <Road radius=".001" color="purple" />
         </Layer>
       </Stage>
     );
